15cb9553158e9dba7fda45c98e83fa110b08e36a
strangeforloop <annaagoha@gmail.com>
2021-01-25 21:09:53 -0800

    add functionality and tests for App component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import './App.css';
 import Controls from './components/Controls';
 import Display from './components/Display.js';
 
+const MAX_NUM__OF_STRIKES = 2;
+const MAX_NUM__OF_BALLS = 3;
+
 function App() {
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
 
   const scoreStrike = () => {
-    setStrikes(strikes + 1);
+    if (strikes === MAX_NUM__OF_STRIKES) {
+      // struck out
+      setStrikes(0);
+      setBalls(0);
+    } else {
+      setStrikes(strikes + 1);
+    }
   };
 
   const scoreBall = () => {
-    setBalls(balls + 1);
+    if (balls === MAX_NUM__OF_BALLS) {
+      // struck out
+      setStrikes(0);
+      setBalls(0);
+    } else {
+      setBalls(balls + 1);
+    }
   };
 
   const scoreFoul = () => {
-    setStrikes(strikes + 1);
+    if (strikes !== MAX_NUM__OF_STRIKES) {
+      setStrikes(strikes + 1);
+    }
   };
 
   const scoreHit = () => {
+    // reset game
     setStrikes(0);
     setBalls(0);
   };
--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,48 @@ describe('<App />', () => {
   it('renders without crashing', () => {
     render(<App />);
   });
+  it('One Big Test', () => {
+    const { getByText } = render(<App />);
+    const strikeBtn = getByText(/^strike$/i);
+    const ballBtn = getByText(/^ball$/i);
+    const foulBtn = getByText(/^foul$/i);
+    const hitBtn = getByText(/^hit$/i);
+
+    // Test that strike works
+    fireEvent.click(strikeBtn);
+    fireEvent.click(strikeBtn);
+    getByText(/Strikes: 2/);
+
+    // Test that ball works
+    fireEvent.click(ballBtn);
+    fireEvent.click(ballBtn);
+    fireEvent.click(ballBtn);
+    getByText(/Balls: 3/);
+
+    // Test that strikes and balls roll over
+    fireEvent.click(strikeBtn);
+    fireEvent.click(strikeBtn);
+    fireEvent.click(ballBtn);
+    fireEvent.click(ballBtn);
+    fireEvent.click(ballBtn);
+    fireEvent.click(ballBtn);
+    getByText(/Strikes: 0/);
+    getByText(/Balls: 0/);
+
+    // Test that foul doesn't increase strikes past 2
+    fireEvent.click(foulBtn);
+    getByText(/Strikes: 1/);
+    fireEvent.click(foulBtn);
+    getByText(/Strikes: 2/);
+    fireEvent.click(foulBtn);
+    getByText(/Strikes: 2/);
+
+    // Test that hit resets the counts of balls and strikes
+    fireEvent.click(ballBtn);
+    fireEvent.click(hitBtn);
+    getByText(/Strikes: 0/);
+    getByText(/Balls: 0/);
+  });
   // it('One Big Test', () => {
   //   const {getByText} = render(<App />);
   //   const strike = getByText(/^strike$/i);
